24201a2b7f43b7b2cf8797c4bd8be1d6c433dcda
Quentin Young <qlyoung@cumulusnetworks.com>
2019-11-20 19:37:46 -0500

    bgpd ultra fast wagyu mode
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- bgpd/bgp_packet.c
+++ bgpd/bgp_packet.c
@@ -1383,6 +1383,7 @@ static int bgp_open_receive(struct peer *peer, bgp_size_t size)
 	if (ret < 0)
 		return BGP_Stop;
 
+#ifndef FUZZING
 	/* Get sockname. */
 	if (bgp_getsockname(peer) < 0) {
 		flog_err_sys(EC_LIB_SOCKET,
@@ -1390,6 +1391,7 @@ static int bgp_open_receive(struct peer *peer, bgp_size_t size)
 			     __func__, peer->host);
 		return BGP_Stop;
 	}
+#endif
 
 	/* Verify valid local address present based on negotiated
 	 * address-families. */
@@ -1427,7 +1429,9 @@ static int bgp_open_receive(struct peer *peer, bgp_size_t size)
 #endif
 		}
 	}
+#ifndef FUZZING
 	peer->rtt = sockopt_tcp_rtt(peer->fd);
+#endif
 
 	return Receive_OPEN_message;
 }
